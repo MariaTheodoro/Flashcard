895a15630e823970fcbf0c4859712fa987286611
MariaTheodoro <00001128788779sp@al.educacao.sp.gov.br>
2024-11-18 19:59:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Raiz ou Nutella?',
+    'O que significa M.A.G.I.C?',
+    'Maredith Grey, Alex Karev, George Omalley, Izzie Stevens e Cristina Yang.'
+)
+
+criaCartao(
+    'Você sabe?',
+    'Qual a especialidade de Cristina Yang?',
+    'Cirurgiã cardiotorácica.'
+)
+
+criaCartao(
+    'Choro e sofrimento.',
+    'Quem era o 007?',
+    'George Omalley.'
+)
+
+criaCartao(
+    'Shipper',
+    'Qual o significado do post it azul?',
+    'O casamento de Derek com Meredith.'
+)
